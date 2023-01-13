221f0a14235e3efe6f3ca151a7a5d20b248a67b7
slithery0 <iamrub3n@gmail.com>
2023-01-13 11:04:25 +0545

    discouraging env files

--- chatgpt-cli.py
+++ chatgpt-cli.py
@@ -4,15 +4,6 @@ import os
 import termcolor
 from prompt_toolkit import prompt
 
-def set_env_vars(file_path):
-    with open(file_path) as f:
-        for line in f:
-            key, value = line.strip().split('=')
-            os.environ[key] = value
-
-# call the function to set environment variables
-set_env_vars('.env')
-
 # import api key
 api_key = os.environ.get("OPENAI_API_KEY")
 openai.api_key = api_key
