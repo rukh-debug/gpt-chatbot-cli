2b6b847d1e28efa78bd7879a02e98582212f2db6
slithery0 <iamrub3n@gmail.com>
2023-04-06 21:45:33 +0545

    Payload added for models, and gpt4 feature enabled

--- src/payloads.py
+++ src/payloads.py
@@ -34,3 +34,13 @@ presets = {
         }
     }
 }
+
+# generate response
+chat_complitions_models = [
+    "gpt-3.5-turbo",
+    "gpt-3.5-turbo-0301",
+    "gpt-4",
+    "gpt-4-0314",
+    "gpt-4-32k",
+    "gpt-4-32k-0314",
+]
