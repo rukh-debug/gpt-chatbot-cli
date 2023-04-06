9ddf7019ed2c91ad1cc35f2b4510465090e7845e
slithery0 <iamrub3n@gmail.com>
2023-04-07 04:15:23 +0545

    Removed db folder from tracking

--- src/history.py
+++ src/history.py
@@ -4,10 +4,11 @@ def chat_history_picker():
   chat_history = get_all_chat_history()
   if (len(chat_history) == 0):
     print("No chat history found")
-    return False
+    exit()
   else:
     print("Pick a chat history:")
     for i in range(len(chat_history)):
-      print(f"{i+1}. {chat_history[i]['preset']}")
+      print(f"{i+1}. {chat_history[i]['title']}")
     chat_history_id = int(input("Enter a number: "))
     return chat_history[chat_history_id-1]
+
